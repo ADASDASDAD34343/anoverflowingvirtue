8a5c410501a3f72525c1436ea92efa363ac152ba
ADASDASDAD34343 <107790054+ADASDASDAD34343@users.noreply.github.com>
2022-12-26 19:02:39 +0900

    돔을위용해서 html 객체저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,11 @@
 "use strict"
-console.log("로그")
+const id =document.querySelector("#id")
+const psword = document.querySelector("#psword")
+const loginBtn =document.querySelector("button")
+loginBtn.addEventListener("click",login)
+ function login(){
+  const req = {
+    id:id.valuel,
+    psword:psword.valuel
+  }
+  fetch(req); }
