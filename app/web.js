0e03581b2c4d65c33370f685f647b1c89ccae4f8
ADASDASDAD34343 <107790054+ADASDASDAD34343@users.noreply.github.com>
2022-12-26 18:36:31 +0900

    파일최적화 js 열결

--- app/web.js
+++ app/web.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home")
 
 app.use("/",home); 
 app.use("/login",home); 
+app.use(express.static(`${__dirname}/src/public`))
 module.exports= app;
 app.set("views", "./src/views")
 app.set("view engine", "ejs")
