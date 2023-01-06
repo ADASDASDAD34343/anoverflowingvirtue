ef917d5fed794e1dc1507140758350980e85af38
ADASDASDAD34343 <107790054+ADASDASDAD34343@users.noreply.github.com>
2023-01-06 13:28:20 +0900

    개인정보 처리 방침추가

--- web.js
+++ web.js
@@ -23,7 +23,34 @@ app.listen(8080, ()=> {
 
 
 
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
 
+// Create connection to database
+var config = {
+  server: '211.199.249.218', 
+  authentication: {
+      type: 'default',
+      options: {
+          userName: 'root', // update me
+          password: 'a_zse4123!@#' // update me
+      }
+  },
+  options: {
+      database: 'SampleDB'
+  }
+}
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected');
+  }
+});
 
 
 
