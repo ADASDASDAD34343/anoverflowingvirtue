df4f0a9ce1016577b221ac248b5f42edd70b40b0
ADASDASDAD34343 <107790054+ADASDASDAD34343@users.noreply.github.com>
2023-01-08 13:27:35 +0900

    db 내용삭제

--- web.js
+++ web.js
@@ -29,29 +29,5 @@ var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
 
 // Create connection to database
-var config = {
-  server: '211.199.249.218', 
-  authentication: {
-      type: 'default',
-      options: {
-          userName: 'root', // update me
-          password: 'a_zse4123!@#' // update me
-      }
-  },
-  options: {
-      database: 'SampleDB'
-  }
-}
-var connection = new Connection(config);
-
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Connected');
-  }
-});
-
 
 
